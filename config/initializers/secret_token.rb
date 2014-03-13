5f51bfed32780694b635b064b3b814537e3335fd
jeffrossif <tuxpqp@gmail.com>
2014-03-13 11:34:49 -0300

    add project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GiftME::Application.config.secret_token = 'c08bbcdce355b9ddde7d62079541e884f6fa1281361f67e98a694bd3f4d0083a5252d5c0e721fe37d01e4e312eb5b3cae8e42f2c25f6d841e55aae02adef1752'
