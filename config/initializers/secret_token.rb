6f6aadc7a60a62c674381093b62b0b3eb38ea9a9
jeffrossif <tuxpqp@gmail.com>
2014-03-17 15:24:00 -0300

    add another project

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-GiftME::Application.config.secret_token = 'c08bbcdce355b9ddde7d62079541e884f6fa1281361f67e98a694bd3f4d0083a5252d5c0e721fe37d01e4e312eb5b3cae8e42f2c25f6d841e55aae02adef1752'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GiftME::Application.config.secret_key_base = 'b71bb170ffaa410b1ce8c99ac103c5d8ac8b636931a84488daf296c134a341fef61ec46272bf10d7dcee3a1beeab1259bf5c6373f2cc4b6d80af8a663a101139'
